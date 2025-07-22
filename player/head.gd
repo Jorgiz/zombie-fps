9538eddbf5fa1de700818f15b3da1e534139e745
Blu <jorginrdt@gmail.com>
2025-07-21 22:20:43 -0300

    Added Movement

--- player/head.gd
+++ player/head.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
+@export var head: Node3D
+
 var sensibility: float = .2
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _physics_process(delta: float) -> void:
+	self.global_position = head.global_position
 
 
 func _unhandled_input(event: InputEvent) -> void:
